64ee971f96064d31bcd55b047d035d788dc6db90
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-22 18:27:27 +0000

    Find low charcters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * _islower - checks for lowercase letters
  * @c: interger to check
- * Return: Void.
+ * Return: (1) if charcter is lower and (0) if not.
  */
 
 int _islower(int c)
 {
 
-if (c >= 'a' && c <= 'z')
+if (c >= 97 && c <= 122)
 {
 return (1);
 }

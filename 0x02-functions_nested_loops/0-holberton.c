ac22b2a90331f1a797209aba8c9a5c701ede49de
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-22 12:50:29 +0000

    Print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,18 @@
  * Return: Always 0 (Success)
  */
 
+
+
 int main(void)
 {
 char *hol_name = "Holberton";
-int  i = 0;
-for (i = 0; i <= 9; i++)
+int i = 0;
+
+for (i = 0; i <= 8; i++)
 {
 _putchar(hol_name[i]);
 }
 _putchar('\n');
+
 return (0);
 }

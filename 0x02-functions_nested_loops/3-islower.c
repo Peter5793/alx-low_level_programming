01703188bd72e3bb04cac10e84aa5e0632e2d7f6
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-22 18:07:52 +0000

    Lower case check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower(int c); - Lower case character check
+ *
+ * Return: (1) if charcter is lower and (0) if not
+ */
+
+int _islower(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}

b74aa46607067ed5396397e572f981044cd3a856
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-22 17:27:05 +0000

    Print in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet on 10 lines
+ *
+ * Return: Void.
+ */
+
+void print_alphabet_x10(void)
+{
+
+int i;
+char alpha;
+
+for (i = 0; i <= 10; i++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+}
+}

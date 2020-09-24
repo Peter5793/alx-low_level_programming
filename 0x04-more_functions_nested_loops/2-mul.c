739c7992793c50ab6487fdc8674adda2ef6e59e4
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-24 13:20:02 +0000

    Multiplication of numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two numbers
+ * @a : first interger
+ * @b : second interger
+ * Return: multiplication
+ */
+
+int  mul(int a, int b)
+{
+return (a * b);
+}

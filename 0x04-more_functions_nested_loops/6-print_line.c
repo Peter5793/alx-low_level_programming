66b44b059707f2738ff8e0eabf96fd5bf8913739
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-24 18:05:19 +0000

    Print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_line - print a straight line code
+ * @n: number of  times the character '_' should be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar('-');
+}
+_putchar('\n');
+}

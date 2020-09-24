3f18aa109a1e1af1c5305633bc9c65f57d901f4c
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-24 20:47:41 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - 1 - 100 with conditions
+ *
+ * Return: Void
+ */
+
+int main(void)
+{
+  int i;
+
+  for (i = 0; i <= 101; i++)
+    {
+      if ((i % 3 == 0) && (i % 5 == 0))
+	{
+	  printf("FizzBuzz");
+	}
+      else if (i % 3 == 0)
+	{
+	  printf("Fizz");
+	}
+      else if (i % 5 == 0)
+	{
+	  printf("Buzz");
+	}
+      else
+	printf("%d", i);
+
+      if (i < 100)
+	{
+	  putchar(' ');
+
+	}
+
+    }
+  putchar('\n');
+
+  return (0);
+}

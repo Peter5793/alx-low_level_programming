d3682a8eced90c53ec7e16498cf99b5a50e23df7
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-28 18:46:21 +0000

    Swap two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 38;
+}

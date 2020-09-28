a14cef8d33d05571dff9d63d1be23a89cb1762af
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-28 19:42:04 +0000

    Print the lenght of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return the length of the character
+ * @s: specific string
+ *
+ * Return: Void
+ */
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; ++i)
+{
+}
+return (i);
+}
+

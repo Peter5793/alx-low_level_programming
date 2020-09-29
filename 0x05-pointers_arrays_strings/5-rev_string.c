38c56a57c415cf4d75e97a9b3a9b40056f70b545
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-29 17:09:01 +0000

    Print in Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse the string
+ * @s: string to be printed
+ * Return: Void
+ */
+
+void rev_string(char *s)
+{
+int x, y, temp = 0;
+while (s[x] != '\0')
+{
+x++;
+}
+x--;
+for (y = 0; x > y; y++, x--)
+{
+temp = s[x];
+s[x] = s[y];
+s[y] = temp;
+}
+}

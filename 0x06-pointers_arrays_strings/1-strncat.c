002c86e44e62059b5da3c58c675e1acf71701c0f
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-10-01 16:26:39 +0000

    Concatnate string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include  "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatnate two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * @n interger  value
+ *
+ * Return: string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+
+  return (dest);
+}
+

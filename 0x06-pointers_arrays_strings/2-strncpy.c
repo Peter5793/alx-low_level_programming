15d20d0cdbbdf57212859e4b52d5a689473321c8
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-10-01 17:35:56 +0000

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - Copy string
+ * @dest: First parameter
+ * @src: second parameter
+ * @n: number of bytes
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')/** Stop at the null character*/
+{
+i++;
+dest[i] = src[i]; /**copy the characters of dest to src */
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}

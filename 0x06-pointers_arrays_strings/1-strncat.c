534cbcb19e9fb665a7474811adbb2a88757c30ca
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-10-01 16:36:54 +0000

    Concatnate tow strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,14 +5,27 @@
  * _strncat - concatnate two strings
  * @dest: first parameter
  * @src: second parameter
- * @n interger  value
+ * @n: bytes
  *
  * Return: string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
-
-  return (dest);
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+j++;
+i++;
+dest[i] = src[j];
+}
+dest[i] = '\0';
+return (dest);
 }
 

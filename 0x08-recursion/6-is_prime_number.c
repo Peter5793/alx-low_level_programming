e7288f22b2090a64ac657ace6f38c8986874b86e
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-10-07 20:56:47 +0000

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - find the prime number
+ * @n : interger
+ * Return: 1 if function is a prime number
+ */
+
+int is_prime_number(int n)
+{
+return (_prime(n, n - 1));
+}
+/**
+ * _prime - checks if the input is a prime number
+ * @n: interger
+ * @div: divisor
+ * Return: 1 if prime number
+ */
+int _prime(int n, int div)
+{
+if (div == 0 && n < 0)
+{
+return (0);
+}
+if (n == 2)
+{
+return (1);
+}
+if (n % div == 0)
+{
+return (0);
+}
+if (div == 2 && n % div == 1)
+{
+return (1);
+}
+return (_prime(n, div - 1));
+}

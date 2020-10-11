59b85cd97c6448e965f7f7306b04c56b2eceac91
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-10-11 16:31:35 +0000

    Write a program that prints the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print name foloowed by new function
+ * @argc: count argument
+ * @argv: vector argument
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}

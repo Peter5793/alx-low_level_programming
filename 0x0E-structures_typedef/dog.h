da39a7f0c0edb62dd2965fa3419bc336d42c23bd
Peter5793 <masaba.peter68@gmail.com>
2020-10-19 23:29:54 +0300

    Creates new structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,8 @@ char *owner;
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
 #endif

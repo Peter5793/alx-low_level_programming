5d8fdfc81e31e096dae15017e65033df33006b72
Peter5793 <masaba.peter68@gmail.com>
2020-10-21 20:31:26 +0300

    Print name usinf function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - funtion that prints name
+ * @name: pointer to the character
+ * @f: pointer to the function
+ */
+void print_name(char *name, void(*f)(char *))
+{
+(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+#include <stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif

8e10d8027a4fbb30fb296e888c06ddd729a79bd5
Peter5793 <masaba.peter68@gmail.com>
2020-10-31 20:45:03 +0300

    Print the elements in a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,5 +19,6 @@ typedef struct list_s
   struct list_s *next;
 } list_t;
 void liebre_tortuga(void) __attribute__ ((constructor));
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif

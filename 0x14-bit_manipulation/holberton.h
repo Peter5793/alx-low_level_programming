3e4a4464ccd5c944d8d8e8ca04fcb25b2d29aaa6
Peter5793 <masaba.peter68@gmail.com>
2020-11-05 22:02:36 +0300

    sets the value of abit to 1 at a given index

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,8 @@
 #define HOLBERTON_H
 #include <stdlib.h>
 
+
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int _putchar(char c);

5040c4f14b9e8263f255475ebccb5c20268f0d33
Peter5793 <masaba.peter68@gmail.com>
2020-11-05 22:39:47 +0300

    Flip bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * flip_bits - counts the number of bits to change
+ * to get ine number to another
+ * @n: number to start
+ * @m: numbers to go through
+ * Return: the numbers of flips
+ **/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int index, flip;
+
+	flip = 0;
+	for (index = 0; index < 64; index++)
+	{
+		if ((m >> index & 1) ^ (n >> index & 1))
+			flip++;
+	}
+	return (flip);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,8 @@
 #define HOLBERTON_H
 #include <stdlib.h>
 
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);

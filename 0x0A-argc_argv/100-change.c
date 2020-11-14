1dee89fa3adb2e7e884fc1227e687047c8cc4692
Peter5793 <masaba.peter68@gmail.com>
2020-11-14 21:34:46 +0300

    Fix with Betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,24 +15,24 @@ int coins(int x);
  */
 int main(int argc, char *argv[])
 {
-  int money;
+	int money;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  money = atoi(argv[1]);
-  if (money < 0)
-    {
-      printf("0\n");
-      return (1);
-    }
-  else
-    {
-      printf("%d\n", coins(money));
-      return (0);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	if (money < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", coins(money));
+		return (0);
+	}
 }
 /**
  * coins - transforms a number into coins (25, 10, 5, 2, 1)
@@ -41,17 +41,17 @@ int main(int argc, char *argv[])
  */
 int coins(int x)
 {
-  if (x == 0)
-    return (0);
-  else if (x / 25 > 0)
-    return (coins(x % 25) + x / 25);
-  else if (x / 10 > 0)
-    return (coins(x % 10) + x / 10);
-  else if (x / 5 > 0)
-    return (coins(x % 5) + x / 5);
-  else if (x / 2 > 0)
-    return (coins(x % 2) + x / 2);
-  else if (x / 1 > 0)
-    return (coins(x % 1) + x / 1);
-  return (0);
+	if (x == 0)
+		return (0);
+	else if (x / 25 > 0)
+		return (coins(x % 25) + x / 25);
+	else if (x / 10 > 0)
+		return (coins(x % 10) + x / 10);
+	else if (x / 5 > 0)
+		return (coins(x % 5) + x / 5);
+	else if (x / 2 > 0)
+		return (coins(x % 2) + x / 2);
+	else if (x / 1 > 0)
+		return (coins(x % 1) + x / 1);
+	return (0);
 }

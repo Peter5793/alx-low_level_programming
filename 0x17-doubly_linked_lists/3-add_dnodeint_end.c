29c9e5810326454be469d017dee73278bee568d1
Peter5793 <masaba.peter68@gmail.com>
2020-12-14 10:45:01 +0300

    Add node to the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - adds a new node to the start of the list
+ * add_dnodeint_end - adds a new node to the end  of the list
  * @head: pointer to the first node
  * @n: interger to be copied
  * Return: NULL if fail
@@ -16,17 +16,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (*head == NULL)
+	if (!*head)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
 
-	while (temp->next)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
 	new_node->prev = temp;
 	temp->next = new_node;
-	return (*head);
+	return (new_node);
 }

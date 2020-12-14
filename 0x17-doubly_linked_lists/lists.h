99eae85ecc7bb957a704f39227547aec8d7ab106
Peter5793 <masaba.peter68@gmail.com>
2020-12-14 08:54:26 +0300

    Restore

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -22,9 +22,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+
 size_t print_dlistint(const dlistint_t *h);
-
-
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 
 

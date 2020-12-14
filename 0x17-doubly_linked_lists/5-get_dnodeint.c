f22d4c985145990f716145446db49961896e2fc2
Peter5793 <masaba.peter68@gmail.com>
2020-12-14 11:36:25 +0300

    Returns nth node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * @head: pointer to the first node
  * @index: node to be returned
  * Return: address of the node NUll if it does not exist
- **/
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;

498e8b17da7d4047b159204d3d46aaf42bef4a91
Peter5793 <masaba.peter68@gmail.com>
2020-12-14 09:16:07 +0300

    Add new node

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node to the start of the list
+ * @head: pointer to the first node
+ * @n: interger to be copied
+ * Return: NULL if fail
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *new_node;
+
+	temp = *head;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+	return (*head);
+}

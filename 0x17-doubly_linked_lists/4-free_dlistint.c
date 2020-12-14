dea25d4da6ae5e8c6ff57a9893a6a5368aded160
Peter5793 <masaba.peter68@gmail.com>
2020-12-14 11:01:58 +0300

    Free node

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,7 +3,7 @@
  * free_dlistint - frees memory from double inked list
  * @head: Pointer to the first node
  * Return: Void
- */
+ **/
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *next, *current;

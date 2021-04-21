c5f17162f32dd773d5a62aa9eed92c60c899cde7
Peter5793 <masaba.peter68@gmail.com>
2021-04-21 23:15:03 +0300

    binary search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@
  */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_rec(int *array, size_t l, size_t r, int value);
 
 #endif

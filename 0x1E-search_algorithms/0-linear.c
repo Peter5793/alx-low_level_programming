d87c790cc6999898881c821ba751d63d97192026
Peter5793 <masaba.peter68@gmail.com>
2021-04-21 09:02:39 +0300

    fix betty errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,20 +3,21 @@
  * linear_search - searches for a value using linear algorithms
  * @array : pointer to the first element
  * @size: number of elements in array
- * @value: value to search for 
+ * @value: value to search for
  * Return: not present return NULL or -1
  */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	if (array == NULL)
 		return (-1);
 	for (; i < size; i++)
 	{
 		printf("Value checked ar array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-				return i;
+				return (i);
 	}
-	return -1;
+	return (-1);
 }
